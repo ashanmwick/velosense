ca5e69f1a855e7274bf8b044bb97d6566a666d1f
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2023-04-10 16:32:45 +0530

    Update main.c
    multithreading added with sample 2 task

--- main.c
+++ main.c
@@ -2,53 +2,101 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_Sensor.h>
- #include <Adafruit_BMP085.h>
+#include <Adafruit_BMP085.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 Adafruit_BMP085 bme;
 
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
+// LED pins
+
 void setup() {
   Serial.begin(115200);
-
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
+    for (;;)
+      ;
   }
-  
-  bool status = bme.begin(0x77);  
+
+  bool status = bme.begin(0x77);
   if (!bme.begin()) {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
+    while (1)
+      ;
   }
-  
+
   delay(2000);
   display.clearDisplay();
   display.setTextColor(WHITE);
+
+
+  //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
+  xTaskCreatePinnedToCore(
+    Task1code, /* Task function. */
+    "Task1",   /* name of task. */
+    10000,     /* Stack size of task */
+    NULL,      /* parameter of the task */
+    1,         /* priority of the task */
+    &Task1,    /* Task handle to keep track of created task */
+    0);        /* pin task to core 0 */
+  delay(500);
+
+  //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
+  xTaskCreatePinnedToCore(
+    Task2code, /* Task function. */
+    "Task2",   /* name of task. */
+    10000,     /* Stack size of task */
+    NULL,      /* parameter of the task */
+    1,         /* priority of the task */
+    &Task2,    /* Task handle to keep track of created task */
+    1);        /* pin task to core 1 */
+  delay(500);
+}
+
+//Task1code: blinks an LED every 1000 ms
+void Task1code(void* pvParameters) {
+  Serial.print("Task1 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for (;;) {
+    Serial.print("This is Task 01 High\n in core ");
+    Serial.println(xPortGetCoreID());
+    delay(1000);
+    Serial.print("This is Task 01 Low\n");
+    delay(1000);
+  }
+}
+
+//Task2code: blinks an LED every 700 ms
+void Task2code(void* pvParameters) {
+  Serial.print("Task2 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for (;;) {
+    display.clearDisplay();
+    // display temperature
+    display.setTextSize(1);
+    display.setCursor(0, 0);
+    display.print("Temperature: ");
+    display.setTextSize(2);
+    display.setCursor(0, 10);
+    display.print(String(bme.readTemperature()));
+    display.print(" ");
+    display.setTextSize(1);
+    display.cp437(true);
+    display.write(167);
+    display.setTextSize(2);
+    display.print("C");
+    display.display();
+    delay(1000);
+  }
 }
 
 void loop() {
-  display.clearDisplay();
-  // display temperature
-  display.setTextSize(1);
-  display.setCursor(0,0);
-  display.print("Temperature: ");
-  display.setTextSize(2);
-  display.setCursor(0,10);
-  display.print(String(bme.readTemperature()));
-  display.print(" ");
-  display.setTextSize(1);
-  display.cp437(true);
-  display.write(167);
-  display.setTextSize(2);
-  display.print("C");
-  
-
-  
-  display.display();
-
-  delay(1000);
 }
